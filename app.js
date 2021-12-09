9f8e25b2915f1ca4032231217d15a32eea8e3df5
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2021-12-09 17:39:30 -0600

    Working on implementing app

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 // import functions
-import { score, generateThrows } from '../game.js'; 
+import { score, generateThrows } from '../game.js';
 // grab DOM elements
 
 // set event listeners
-    // get user input
-    // use user input to update state
+// get user input
+const playerInput = document.getElementById('button');
+const mainDisplay = document.getElementById('main');
+// use user input to update state
+playerInput.addEventListener('input', () => {
+    mainDisplay.textContent = playerInput.value;
+}
     // update DOM to reflect the new state
+    mainDisplay.addEventListener('change', () => {
+    playerChoice = playerInput.value;
+}
+
+//images and audio
+const audio = document.getElementById('audio');
+
+const rockSound = document.getElementById('rock-sound');
+rockSound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/rock.mp3';
+    audio.play();
+});
+
+const paperSound = document.getElementById('paper-sound');
+paperSound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/paper.mp3';
+    audio.play();
+});
+
+const scissorsSound = document.getElementById('scissors-sound');
+scissorsSound.addEventListener('click', () => {
+    audio.src = 'assets/sounds/scissors.mp3';
+    audio.play();
+});
--- game.js
+++ game.js
@@ -37,9 +37,13 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 const throws = ['rock', 'paper', 'scissors'];
-
+//computer throws
 export function generateThrows(){
     const index = getRandomIntInclusive(0, 2);
     return throws[index];
 
+}
+export function playerThrows(){
+    const player = document.getElementById('button');
+    return player; 
 }
